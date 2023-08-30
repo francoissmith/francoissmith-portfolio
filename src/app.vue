c53c045322ad47b465a881cf8fcd12d69859a5d1
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-08-30 10:57:06 +0200

    added images

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="bg-blue-500 text-white p-4">
-    This is a Nuxt project with Tailwind CSS!
+  <div class="w-full h-full p-4 flex justify-center">
+    <Card>
+      <template #bg>
+        <img src="" alt="">
+      </template>
+    </Card>
   </div>
 </template>
+
+<script>
+import Card from '@/components/Card.vue';
+
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-2 w-full h-full max-w-5xl max-h-60 rounded-3xl hover:scale-[1.005] shadow hover:-translate-y-1 relative transition-all duration-300">
+        <div class="absolute w-full h-full brightness-95 rounded-3xl top-0 left-0">
+            <slot name="bg"></slot>
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
